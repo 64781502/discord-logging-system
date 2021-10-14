554533c3e6a5866e5d69e3e6e48eb0616f0f5c7e
Andrew <60436090+TheRealOXY@users.noreply.github.com>
2021-10-14 22:30:07 +0200

    Update req.js

--- req.js
+++ req.js
@@ -4,14 +4,11 @@ var data = new FormData();
 function sendMessage() {
 	let channelid = document.getElementById("channelid").value;
 	var ServerURL = 'https://discord.com/api/v9/channels/' + channelid + '/messages';
-	var nonce = Math.floor(Math.random() * (9999999999999999999 - 1000000000000000000 + 1)) + 1000000000000000000;
 	var content = document.getElementById("content").value;
 	var mfa = document.getElementById("mfa").value;
-	console.log('One-time nonce created: ' + nonce);
 	console.log('Message content: ' + content);
 	console.log('Authorization: ' + mfa);
-    data.append('content', content);
-  	data.append('nonce', nonce);
+    	data.append('content', content);
 	xhr.open('POST', ServerURL, true);
   	xhr.setRequestHeader('authorization', mfa);
 	xhr.onload = function(){console.log(this.responseText);if(mfa!=''){document.getElementById("btn").style.backgroundColor='#adffad'}else{document.getElementById("btn").style.backgroundColor='#ffadad'}};
